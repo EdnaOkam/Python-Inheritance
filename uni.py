03fffedf0c936c062b7318514c9edd4df87a3556
EdnaOkam <achiokam5@yahoo.com>
2024-02-01 21:49:11 +0100

    Created University Class

---
+++ uni.py
@@ -0,0 +1,62 @@
+class Person:
+    def __init__ (self, name, age):
+        self.name = name
+        self.age = age 
+
+def display_info(self):
+    print(f"Name: {self.name} \n Age: {self.age}")
+
+class Student(Person):
+    def __init__ (name, age, student_id, courses, assignments_due=0 ):
+        super().__init__( name, age)
+        self.student_id = student_id
+        self.courses = []
+        self.assignments_due = assignments_due = 0 
+
+def display_info(self):
+    super().display_info()
+    print(f"Student ID: {self.student_id} \nCourses taken {[self.courses]} \nAssignments Due: {self.assignments_due}")
+
+def submit_assignment(assignment_name):
+    assignment_name = self.assignments_due
+    print(f"Student {self.name} submitted assignment : {assignment_name}")
+
+def attend_class():
+    self.student_id = self.courses
+    print(f"Student {self.name} attends {self.courses} classes.")
+
+class Teacher(Person):
+    def __init__ (name, age, employee_id, courses_taught, office_hours ):
+        super(). __init__ (name, age)
+        self.employee_id = employee_id
+        self.courses_taught = []
+        self.office_hours = office_hours
+
+def display_info():
+    super().display_info()
+    print(f"Employee ID: {self.employee_id} \nCourses Taught: {self.courses_taught} \nOffice Hours: {self.office_hours}")
+
+def schedule_office_hours(day,time):
+    
+    # {
+        # 'day':['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+        # 'time': [9, 10, 11, 1, 2, 3, 4]
+        # }
+    print(f"Scheduled Office Hours for Employee ID : {self.employee_id} : {days} at {time}")
+
+# person = Person("Ezra", 18)
+# person.display_info()
+
+student1 = Student("Ezra" , 18, "23456", ["Physics", "Math", "Biology"])
+student1.display_info()
+student1.submit_assignment("Physics Assignment 1")
+student1.attend_class()
+
+teacher1 = Teacher("Mr. David", 34," 45623", ["Physic", "Math"] )
+teacher1.schedule_office_hours("Monday" "Wednesday", "9:00 AM - 11:00 AM", "1:00 PM - 2:00 PM ")
+teacher1.display_info()
+
+
+    
+
+
