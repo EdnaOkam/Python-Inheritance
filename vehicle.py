7d673bd906a22b9634549f6337aef179cbb77b80
EdnaOkam <achiokam5@yahoo.com>
2024-02-07 14:32:45 +0100

    debugged vehicle class

--- vehicle.py
+++ vehicle.py
@@ -1,5 +1,5 @@
 class Vehicle:
-    def _init_(self, make, model, year):
+    def __init__(self, make, model, year):
         self.make = make
         self.model = model 
         self.year = year
@@ -8,7 +8,7 @@ class Vehicle:
         print(f"Vehicle Make: {self.make} \nVehicle Model: {self.model} \nYear of Production: {self.year}")
 
 class EngineMixin:
-    def _init_(self, compression_ratio):
+    def __init__(self, compression_ratio):
         self.compression_ratio = compression_ratio
 
     def display_info(self):
@@ -18,7 +18,7 @@ class EngineMixin:
         print("Engine has started")
 
 class ElectricMixin:
-    def _init_ (self, charge_level):
+    def __init__ (self):
         self.charge_level = 0
 
     def charge(self,percentage):
@@ -27,8 +27,8 @@ class ElectricMixin:
         return percentage
 
 class Car(Vehicle,EngineMixin):
-    def _init_(self, make, model, year, nuum_doors, num_passengers):
-        super()._init_(make, model, year)
+    def __init__(self, make, model, year, nuum_doors, num_passengers):
+        super().__init__(make, model, year)
         self.nuum_doors = nuum_doors
         self.num_passengers = num_passengers
 
@@ -37,8 +37,9 @@ class Car(Vehicle,EngineMixin):
         print(f"Number of Doors in the Car: {self.nuum_doors} \nNumber of Passengers in the Car: {self.num_passengers}")
 
 class ElectricCar(Car,ElectricMixin):
-    def _init_(self, make, model, year, nuum_doors, num_passengers, battery_capacity):
-        super()._init_(self, make, model, year, nuum_doors, num_passengers)
+    def __init__(self, make, model, year, nuum_doors, num_passengers, battery_capacity):
+        super().__init__(make, model, year, nuum_doors, num_passengers)
+        ElectricMixin.__init__(self)
         self.battery_capacity = battery_capacity
 
     def display_info(self):
@@ -46,8 +47,8 @@ class ElectricCar(Car,ElectricMixin):
         print(f"The Battery Capacity of the Electric Car : {self.battery_capacity}")
 
 class Motocycle(Vehicle,EngineMixin):
-    def _init_(self, make, model, year, num_wheels, has_sidecar):
-        super()._init_(make, model, year)
+    def __init__(self, make, model, year, num_wheels, has_sidecar):
+        super().__init__(make, model, year)
         self.num_wheels = num_wheels
         self.has_sidecar = has_sidecar
 
@@ -55,15 +56,16 @@ class Motocycle(Vehicle,EngineMixin):
 # car1 = Vehicle("BMW", "Turismo", 2020)
 # car1.display_info()
 
-engine_info = EngineMixin()
+engine_info = EngineMixin(10)
 engine_info.display_info()
 engine_info.start_engine()
 
 car_info = Car("Nissan", "Altima", 2021, 4, 4,)
 car_info.display_info()
-car_info.charge(75)
+# car_info.charge(75)
 
 electric_car = ElectricCar("Mercedes", "Maybach",2022, 6, 4, "40kWh")
 electric_car.display_info()
+electric_car.charge(75)
 
 moto1 = Motocycle("Ducati", "Alta", 2018, 2, True)
